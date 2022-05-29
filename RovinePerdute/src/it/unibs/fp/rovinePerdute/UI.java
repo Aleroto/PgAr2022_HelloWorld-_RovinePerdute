56e936f1d3db90ece1bbc35b3f7237bd4c638a4b
Oh-V1G0-Rally <76070738+Oh-V1G0-Rally@users.noreply.github.com>
2022-05-29 17:27:21 +0200

    .

--- RovinePerdute/src/it/unibs/fp/rovinePerdute/UI.java
+++ RovinePerdute/src/it/unibs/fp/rovinePerdute/UI.java
@@ -1,10 +1,11 @@
 package it.unibs.fp.rovinePerdute;
+import it.unibs.fp.XML.*;
 
 public class UI {
 	
 	
 	/**Runs the Program*/
 	public static void runProgram(){
-		
+		XMLReader.printXML();
 	}
 }
