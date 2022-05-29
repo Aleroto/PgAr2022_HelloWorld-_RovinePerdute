550edd20b15b28235e2aa34df7931daca31fe1fe
Oh-V1G0-Rally <76070738+Oh-V1G0-Rally@users.noreply.github.com>
2022-05-29 11:52:49 +0200

    First part of class to implement a Graph

---
+++ RovinePerdute/src/it/unibs/fp/rovinePerdute/Position.java
@@ -0,0 +1,37 @@
+package it.unibs.fp.rovinePerdute;
+
+/**
+ * Class to implements in the City object for the position. Cartesian position X and Y
+ * and H for altitude.
+ */
+public class Position {
+
+	private Integer X, Y, H;
+
+	//Getter
+	public Integer getX() {
+		return X;
+	}
+
+	public Integer getY() {
+		return Y;
+	}
+
+	public Integer getH() {
+		return H;
+	}
+
+	//Setter
+	public void setX(Integer x) {
+		X = x;
+	}
+
+	public void setY(Integer y) {
+		Y = y;
+	}
+
+	public void setH(Integer h) {
+		H = h;
+	}
+
+}
